a97c0981a2b9fe7cfe14b984626eaab3cf9cac7d
aleenayasin <99421658+aleenayasin@users.noreply.github.com>
2024-02-22 15:21:31 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// Select and cache the <main> element in a variable named mainEl.
+const mainEl = document.querySelector('main');
+
+// Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+// Set the content of mainEl to <h1>DOM Manipulation</h1>. 
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+
+// Add a class of flex-ctr to mainEl.
+// Hint: Use the Element.classList API.
+mainEl.classList.add('flex-ctr');
+
+
+// Part 2: Creating a Menu Bar
+// Next, create a blank menu bar that we can use to later add some interactivity to the page:
+// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+const topMenuEl = document.getElementById('top-menu');
+
+// Set the height of the topMenuEl element to be 100%.
+topMenuEl.style.height = '100%';
+// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+// Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add('flex-around');
+
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
+  
+  const nav = document.querySelector('nav')
+
+  for (var i = 0; i < menuLinks.length; i++) {
+      var link = menuLinks[i];
+      var menuItem = document.createElement('a');
+      menuItem.textContent = link.text;
+      menuItem.href = link.href;
+      nav.appendChild(menuItem);
+  }
